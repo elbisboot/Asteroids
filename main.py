faf98514f9b12f1e99c6fc2abd48ec1a5eaefa60
elbisboot <elbis7@proton.me>
2025-08-03 22:47:33 +0100

    added rotation

--- main.py
+++ main.py
@@ -12,13 +12,29 @@ def main():
 	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 	while True:
 		screen.fill("black")
-		player.draw(screen)
-		pygame.display.flip()
-		dt = clock.tick(60) / 1000
-			
-		for event in pygame.event.get():
-    			if event.type == pygame.QUIT:
-        			return
+		
+import pygame
+from constants import *
+from player import Player
+def main():
+        print("Starting Asteroids!")
+        print(f"Screen width: {SCREEN_WIDTH}")
+        print(f"Screen height: {SCREEN_HEIGHT}")
+        pygame.init()
+        clock = pygame.time.Clock()
+        dt = 0
+        screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+        while True:
+                dt = clock.tick(60) / 1000
+                screen.fill("black")
+                player.update(dt)
+                player.draw(screen)
+                pygame.display.flip()
+	
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        return
 
 if __name__ == "__main__":
     main()
