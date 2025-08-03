99bb8da9dffd67027abcb8394f8680631762f255
elbisboot <elbis7@proton.me>
2025-08-03 21:46:17 +0100

    added clock

--- main.py
+++ main.py
@@ -5,10 +5,14 @@ def main():
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
 	pygame.init()
+	clock = pygame.time.Clock()
+	dt = 0
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	while True:
 		screen.fill("black")
 		pygame.display.flip()
+		dt = clock.tick(60) / 1000
+			
 		for event in pygame.event.get():
     			if event.type == pygame.QUIT:
         			return
